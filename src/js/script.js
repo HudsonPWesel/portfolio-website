7e95c0a7b1525bd053868cf25740c1671e201339
Hudson Wesel <cswyld7k@gmail.com>
2022-08-24 17:06:13 -0400

    create remaing HTML, JS, PNG, JPG Files

---
+++ src/js/script.js
@@ -0,0 +1,100 @@
+'use-strict';
+const tabItem = Array.from(document.querySelectorAll('.about__tabbed-item'));
+
+const tabIcons = Array.from(
+	document.querySelectorAll('.about__tabbed-item--icon')
+);
+
+const itemText = document.querySelector('.about__tabbed-item--text');
+
+const tabText = Array.from(
+	document.querySelectorAll('.about__tabbed-item--text')
+);
+
+const toggleClass = (elements, index, toggleClass) => {
+	elements[index].classList.toggle(toggleClass);
+};
+
+// let arrowBtn = document.querySelector('.about__tabbed-item--icon');
+
+// arrowBtn.addEventListener('click', () => {
+// 	console.log(arrowBtn.classList[1].slice(-1));
+// });
+
+// const getNumberHTML = elements => {
+// 	elements.forEach((element, index) => {
+// 		console.log(element.classList[1].slice(-1));
+// 		return element.classList[1].slice(-1);
+// 	});
+// };
+
+// const allIcons = getNumberHTML(tabIcons);
+
+// const allTabedText = getNumberHTML(
+// document.querySelectorAll('.about__tabbed-item--text')
+// );
+
+// arrowBtn.classList[1].slice(-1);
+// switch (arrowBtn.classList[1].slice(-1)) {
+// 	case '1':
+// 		arrowBtn = document.querySelector('.about__tabbed-item--icon-1');
+// 		break;
+
+// 	case '2':
+// 		arrowBtn = document.querySelector('.about__tabbed-item--icon-2');
+// 	default:
+// 		arrowBtn = document.querySelector('.about__tabbed-item--icon-3');
+// 		break;
+// }
+
+// console.log(arrowBtn.classList[1].slice(-1));
+
+// console.log(tabText[0].classList.toggle('hidden'));
+
+tabIcons.forEach((element, index) => {
+	element.addEventListener('click', () => {
+		if (index === 0) {
+			// Could Export this to a function
+
+			toggleClass(tabText, index, 'hidden');
+			toggleClass(tabText, index, 'active');
+			tabItem[0].style.gridRowGap = '2.5rem';
+			console.log(tabItem[1]);
+
+			// tabText[0].classList.toggle('hidden');
+			// tabText[0].classList.toggle('active');
+
+			element.style.transform = 'rotate(90deg)';
+
+			if (tabText[0].classList.contains('hidden')) {
+				element.style.transform = null;
+				tabItem[0].style.gridRowGap = 0;
+			}
+		} else if (index === 1) {
+			toggleClass(tabText, index, 'hidden');
+			toggleClass(tabText, index, 'active');
+			element.style.transform = 'rotate(90deg)';
+
+			tabItem[1].style.gridRowGap = '2.5rem';
+
+			// tabText[1].classList.toggle('hidden');
+			// tabText[1].classList.toggle('active');
+
+			if (tabText[1].classList.contains('hidden')) {
+				element.style.transform = null;
+				tabItem[1].style.gridRowGap = 0;
+			}
+		} else if (index === 2) {
+			toggleClass(tabText, index, 'hidden');
+			toggleClass(tabText, index, 'active');
+
+			tabItem[2].style.gridRowGap = '2.5rem';
+
+			element.style.transform = 'rotate(90deg)';
+			if (tabText[2].classList.contains('hidden')) {
+				element.style.transform = null;
+				tabItem[2].style.gridRowGap = 0;
+			}
+		}
+	});
+});
